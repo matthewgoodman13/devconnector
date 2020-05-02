69be6b8ec256c4d91138239f5fc97de77a54432f
Matthew Goodman <matthewgoodman13@outlook.com>
2020-05-02 10:51:25 -0400

    profile schema created

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB Connected...")
-    }
-    catch(err) {
-        console.error(err.message);
-        process.exit(1); // Exit process with failure
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 module.exports = connectDB;
