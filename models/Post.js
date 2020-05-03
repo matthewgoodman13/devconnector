dfdb81fb08e48358924cbd0bcd8a696777c3d712
Matthew Goodman <matthewgoodman13@outlook.com>
2020-05-03 18:50:39 -0400

    redux store

--- models/Post.js
+++ models/Post.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  _id: {
-    type: Object,
-  },
+  // _id: {
+  //   type: Object,
+  // },
   user: {
     type: Schema.Types.ObjectId,
   },
