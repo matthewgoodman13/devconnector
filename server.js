b69551728b97be2aabedc5055c4478fc282655ac
Matthew Goodman <matthewgoodman13@outlook.com>
2020-05-05 19:14:34 -0400

    rm proxy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 const path = require('path');
 
 const app = express();
@@ -8,7 +9,9 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json());
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running.'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,14 +21,19 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  // Set Static Folder
+
+  const publicPath = path.join(__dirname, 'client', 'build');
+
+  app.use(express.static(publicPath));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(publicPath, 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
